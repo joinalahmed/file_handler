6d470c3eb8788407e07060e2705fa15a80497e35
aayush <saayush305@gmail.com>
2017-01-08 14:35:49 +0530

    new

--- intersection.py
+++ intersection.py
@@ -1,2 +1,7 @@
- jhbjh
-
+test_set = [100, 101, 000, 110]
+test_set_case1 = [100, 101]
+for value in range(len(test_set_case1)):
+    temp_var = test_set_case1[value])
+    print(temp_var)
+    for value_s in range(len(temp_var)):
+        print(temp_var)
