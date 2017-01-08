6552a1050d703124ea0b4da614038b442db3e87f
aayush <saayush305@gmail.com>
2017-01-08 16:09:41 +0530

    new

--- intersection.py
+++ intersection.py
@@ -1,9 +1,11 @@
-test_set = [100, 101, 000, 110]
+test_set = [110, 101, 100, 111]
 test_set_case1 = [101, 100]
 test_set_case2 = []
 temp_var = ''
 temp_var1 = ''
 
+test_set.sort()
+test_set_case1.sort()
 # COMPLEMENT
 for value in range(len(test_set_case1)):
     temp_var = ''.join(str(test_set_case1[value]))
@@ -15,10 +17,18 @@ for value in range(len(test_set_case1)):
     test_set_case2.append(temp_var1)
     temp_var = ''
     temp_var1 = ''
+test_set_case2.sort()
 
-print(test_set_case2)
+
 
 # SET COMPARATOR
-for compare in range(len(test_set)):
-
-
+compare_1 = set(test_set).intersection(test_set_case1)
+compare_2 = set(test_set).intersection(test_set_case2)
+var1 = set(test_set_case1)
+var2 = set(test_set_case2)
+if compare_1 == var1:
+    print('test_set_case1 can be used')
+elif compare_2 == var2:
+    print('test_set_case1 can be used')
+else:
+    print('NO MATCH FOUND')
