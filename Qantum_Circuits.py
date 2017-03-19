fc3025f626051b26e701d01cefd4c90e0214e70a
aayush <saayush305@gmail.com>
2017-03-19 15:24:50 +0530

    new

--- Qantum_Circuits.py
+++ Qantum_Circuits.py
@@ -44,10 +44,10 @@ if sel_val is 0:
                 kk.write(line)
                 kk.close()
             file.write('BEGIN' + '\n')
-        main_li_1 = main_li[ val_1:val_2 ]
-        main_li_1 = main_li_1[ ::-1 ]
+        main_li_1 = main_li[val_1:val_2]
+        main_li_1 = main_li_1[::-1]
         for ii in range(len(main_li_1)):
-            temp_1 = str(main_li_1[ ii ])
+            temp_1 = str(main_li_1[ii])
             file.write(temp_1)
         file.write('END' + '\n')
 
