a572eaf481a76e1300de6bc15fd8a9791cc27495
aayush <saayush305@gmail.com>
2017-03-19 15:24:18 +0530

    new

--- Qantum_Circuits.py
+++ Qantum_Circuits.py
@@ -34,18 +34,20 @@ if sel_val is 1:
         file.write('END' + '\n')
 
 # REVERSE SIMULATION BLOCK
-with open('mid.tfc', 'a') as file:
-    with open('main1.tfc', 'r+') as file_1:
-        for line in file_1:
-            if line.strip() == 'BEGIN':
-                break
-            kk = open('mid.tfc', 'a')
-            kk.write(line)
-            kk.close()
-        file.write('BEGIN' + '\n')
-    main_li_1 = main_li[val_1:val_2]
-    main_li_1 = main_li_1[::-1]
-    for ii in range(len(main_li_1)):
-        temp_1 = str(main_li_1[ii])
-        file.write(temp_1)
-    file.write('END' + '\n')
+if sel_val is 0:
+    with open('mid.tfc', 'a') as file:
+        with open('main1.tfc', 'r+') as file_1:
+            for line in file_1:
+                if line.strip() == 'BEGIN':
+                    break
+                kk = open('mid.tfc', 'a')
+                kk.write(line)
+                kk.close()
+            file.write('BEGIN' + '\n')
+        main_li_1 = main_li[ val_1:val_2 ]
+        main_li_1 = main_li_1[ ::-1 ]
+        for ii in range(len(main_li_1)):
+            temp_1 = str(main_li_1[ ii ])
+            file.write(temp_1)
+        file.write('END' + '\n')
+
